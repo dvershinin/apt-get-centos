bf03be93b292ecd3dbb61d267199f7d6665c409f
Danila Vershinin <ciapnz@gmail.com>
2018-08-31 14:40:20 +0300

    README update

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# apt-get-centos
+# apt-get for CentOS
 
+This is a simple `apt-get` interface for managing packages in CentOS. It's going to be useful for CentOS adopters who came from Ubuntu world.
+
+## Installation
+
+    curl https://raw.githubusercontent.com/dvershinin/apt-get-centos/master/apt-get.sh -o /usr/local/bin/apt-get
+    chmod 0755 /usr/local/bin/apt-get
+
+## Usage
+
+Usage is similar to what you would normally type in Ubuntu. Please note, this is convinience learning interface rather than something that you should always use. 
+When you run a `apt-get` command, it will run the equivalent `yum` command.
+
+### Install a package
+
+    apt-get install nginx
+
+### Remove a package
+
+    apt-get remove nginx
+
+## Purge a package
+
+    apt-get purge nginx
--- apt-get.sh
+++ apt-get.sh
@@ -3,6 +3,10 @@
 shopt -s expand_aliases
 alias errcho='>&2 echo'
 
+print_usage () {
+    errcho "Usage: apt-get <command>"
+}
+
 yes=""
 while getopts ":y" opt; do
   case $opt in
@@ -18,7 +22,7 @@ shift $((OPTIND-1))
 
 if [ -z "$1" ]
 then
-    errcho "A command is required."
+    print_usage
     exit 1
 else
     command="$1"
@@ -31,5 +35,29 @@ case $command in
     install )
         yum $yes install $2
         echo "Next time you may want to use: yum install $2"
+    ;;
+    remove ) 
+        yum $yes remove $2
+        echo "Next time you may want to use: yum remove $2"
+    ;;
+    purge )
+	shift;
+	for package in $@
+        do
+            if rpm -q $package > /dev/null ; then
+              echo "removing config files for $package"
+              for file in $(rpm -q --configfiles $package)
+              do
+                echo "  removing $file"
+                # rm -f $file
+              done
+              rpm -e $package
+            else
+              echo "package $package is not installed"
+            fi
+	done
+    ;;
+    *)
+      errcho 'This command is not implemented. Have a look at "man yum" ?'
     ;;
 esac
